eae51b970914216d8b1bb9c52e5551bf7ada70ed
PepeSilvia215 <gideongoddard@protonmail.com>
2020-05-05 05:20:12 +0000

    now lists memes

--- mememachine.py
+++ mememachine.py
@@ -1,4 +1,6 @@
 import discord
+import glob
+import re
 
 client = discord.Client()
 
@@ -6,13 +8,44 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+def getmemefiles():
+    memefiles = []
+    for file in glob.glob("muh_sounds_bruh/**/*.flac", recursive=True):
+            file = file[len("muh_sounds_bruh/"):]
+            file = file[:-(len(".flac"))]
+            memefiles.append(file)
+    memefiles.sort()
+    return memefiles
+
+
+async def listmemes(message):
+    await message.author.send("Here are the available memes:")
+    memefiles = getmemefiles()
+    #print(memefiles)
+    #split into chunks
+    chunksize = 50
+    chunks = [memefiles[i:i + chunksize] for i in range(0, len(memefiles), chunksize)]
+    #print(chunks)
+    for memefileschunk in chunks:
+        mememessage = "\n".join(memefileschunk)
+        await message.author.send(mememessage)
+    await message.channel.send("Boi i have slid into your dms with a list of memes")
+
+
+async def handlemessage(message):
+    if message.content.startswith('##list'):
+        await listmemes(message)
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('##'):
+        await handlemessage(message)
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("goodbye.")
 
 api_key_file = open("api-key","r")
 api_key = api_key_file.readline()
