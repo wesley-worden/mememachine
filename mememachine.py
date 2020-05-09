8c4816287847018795a770d60da3b9af39398bb5
PepeSilvia215 <gideongoddard@protonmail.com>
2020-05-09 05:10:30 +0000

    now uses cogs

--- mememachine.py
+++ mememachine.py
@@ -52,55 +52,62 @@ async def on_ready():
 #    await messagehim("MemeMachine up and running like a dream bruh")
 
 #commands
-@bot.command(brief="plays the requested meme bruh", description="will play the meme in muh sounds bruh that exactly matches")
-async def play(context, meme=""):
-    message = context.message
-    if not meme:
-        await message.channel.send("bruh i need a meme to play")
-        return
-    meme = findmemes(meme, 1)[0]
-    if(meme == []):
-        await message.channel.send("no meme bruh")
-    else:
-        memepath = "/home/pepesilvia/mememachine/muh_sounds_bruh/" + meme + ".flac"
-        await message.channel.send("i wanna play " + memepath)
+class muh_meme_commands_bruh(commands.Cog):
+    """bruh all the meme commands"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(brief="plays the requested meme bruh", description="will play the meme in muh sounds bruh that exactly matches")
+    async def play(context, meme=""):
+        message = context.message
+        if not meme:
+            await message.channel.send("bruh i need a meme to play")
+            return
+        meme = findmemes(meme, 1)[0]
+        if(meme == []):
+            await message.channel.send("no meme bruh")
+        else:
+            memepath = "/home/pepesilvia/mememachine/muh_sounds_bruh/" + meme + ".flac"
+            await message.channel.send("i wanna play " + memepath)
 
 
-@bot.command(brief="returns the integer id for your user bruh", description="returns the integer id for your user bruh, useful for discord debug n shit")
-async def getid(context):
-    message = context.message
-    await message.author.send(str(message.author.id))
+    @commands.command(brief="returns the integer id for your user bruh", description="returns the integer id for your user bruh, useful for discord debug n shit")
+    async def getid(context):
+        message = context.message
+        await message.author.send(str(message.author.id))
 
-@bot.command(brief="search and find up to 25 memes bruh", description="shows the first 25 memes that contain substrings that exactly match bruh")
-async def search(context, query=""):
-    message = context.message
-    if not query:
-        await message.channel.send("try using a search term bruh")
-        return
-    matches = findmemes(query, 25)
-    if(matches == []):
-        await message.channel.send("no sounds bruh")
-    else:
-        matches.insert(0, "")
-        searchmessage = "\n".join(matches)
-        await message.channel.send("bruh are you lookin for ```" + searchmessage + "```") 
+    @commands.command(brief="search and find up to 25 memes bruh", description="shows the first 25 memes that contain substrings that exactly match bruh")
+    async def search(context, query=""):
+        message = context.message
+        if not query:
+            await message.channel.send("try using a search term bruh")
+            return
+        matches = findmemes(query, 25)
+        if(matches == []):
+            await message.channel.send("no sounds bruh")
+        else:
+            matches.insert(0, "")
+            searchmessage = "\n".join(matches)
+            await message.channel.send("bruh are you lookin for ```" + searchmessage + "```") 
 
-@bot.command(brief="sends you all the memes in a massive DM, dont", description="this will tear into your DMs and send you all the available memes, which is so many memes bro. to actually send all the memes use\n##list memes\nthis is done to prevent your destruction")
-async def list(context, password=""):
-    message = context.message
-    if not password:
-        await message.channel.send("bruh i am so full of memes, if you are sure you want this use `##list memes`")
-        return
-    elif(password == "memes"):
-        await message.author.send("here are the available memes:")
-        memefiles = getmemefiles()
-        #split into chunks
-        chunksize = 50
-        chunks = [memefiles[i:i + chunksize] for i in range(0, len(memefiles), chunksize)]
-        for memefileschunk in chunks:
-            mememessage = "\n".join(memefileschunk)
-            await message.author.send(mememessage)
-        await message.channel.send("you will not survive")
+    @commands.command(brief="sends you all the memes in a massive DM, dont", description="this will tear into your DMs and send you all the available memes, which is so many memes bro. to actually send all the memes use\n##list memes\nthis is done to prevent your destruction")
+    async def list(context, password=""):
+        message = context.message
+        if not password:
+            await message.channel.send("bruh i am so full of memes, if you are sure you want this use `##list memes`")
+            return
+        elif(password == "memes"):
+            await message.author.send("here are the available memes:")
+            memefiles = getmemefiles()
+            #split into chunks
+            chunksize = 50
+            chunks = [memefiles[i:i + chunksize] for i in range(0, len(memefiles), chunksize)]
+            for memefileschunk in chunks:
+                mememessage = "\n".join(memefileschunk)
+                await message.author.send(mememessage)
+            await message.channel.send("you will not survive")
+bot.add_cog(muh_meme_commands_bruh(bot))
 #end commands
 
 @bot.event
