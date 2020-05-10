395166cce121cf4fb561cc73432fdd36adf550d1
PepeSilvia215 <gideongoddard@protonmail.com>
2020-05-10 17:50:52 +0000

    anon gif memes and shrek password to kill

--- mememachine.py
+++ mememachine.py
@@ -11,6 +11,7 @@ import glob
 import aiohttp
 import json
 import asyncio
+import random
 #import giphy_client
 #from giphy_client.rest import ApiException
 #import signal
@@ -28,10 +29,20 @@ import asyncio
 prefix = "##"
 memepath = "/home/pepesilvia/mememachine/muh_sounds_bruh/dva-okay.flac"
 #status = "potatoe.exe"
+wednesday_channel = 708554699225301002
 
 #global shit
 playing = False
 endsig = False
+shrek_images = [ "http://churchofshrek.yolasite.com/resources/417930_240580199418458_792918923_n.jpg",
+        "https://i.pinimg.com/564x/cf/8c/f4/cf8cf4528649708b8a022a3bc6cabb01.jpg",
+        "https://i.pinimg.com/236x/24/55/94/245594470ed0704eba0c89654c0d0f57.jpg",
+        "https://cdn.drawception.com/images/panels/2016/8-12/FMSKyT3j6O-2.png",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG5-FXMKGyVzf6TJyCXrl4PQ35S6YSbAQcRoEt2QaMNdTAWqiV&s",
+        "https://pbs.twimg.com/media/CxUhUibUcAAV-tQ.jpg",
+        "https://i.chzbgr.com/full/9125133824/h1CBCE60B/funny-meme-about-shrek-and-worshiping-him" ]
+shrek_words = [ "shrek", "love", "life", "donkey", "swamp", "god", "healer", "holy", "worship", "soul", "light",
+        "redemption", "righteous" ] 
 
 #get giphy key
 giphy_key_file = open("giphy-key", "r")
@@ -41,7 +52,20 @@ giphy_key_file.close()
 #giphy_api_instance = giphy_client.DefaultApi()
 session = aiohttp.ClientSession()
 
+
+#set shreks holy code
+print("*kneels in prayer*")
+print("please shrek, we pray to you, watch over this bot")
+print("let him play the memes for our bretheren and their defenenders,")
+print("and to kill this bot if he betrays your legacy or his master")
+random.shuffle(shrek_words)
+shreks_holy_code = "".join(shrek_words[:4])
+print("\n our holy god answers: " + shreks_holy_code)
+
 #helper functions
+def getrandomshrekimage():
+    return random.choice(shrek_images)
+
 def getapikey():
     api_key_file = open("api-key", "r")
     api_key = api_key_file.readline()
@@ -171,10 +195,12 @@ async def play(context, *args):
         await message.channel.send("bruh you gotta be in a voice channel first")
         return
     #find the meme
-    meme = findmemes(meme, 1)[0]
-    if(meme == []):
-        await message.channel.send("no meme bruh")
-        return
+    memepath = "/home/pepesilvia/mememachine/muh_sounds_bruh/" + meme + ".flac"
+    if not os.path.exists(memepath):
+        meme = findmemes(meme, 1)[0]
+        if(meme == []):
+            await message.channel.send("no meme bruh")
+            return
     memepath = "/home/pepesilvia/mememachine/muh_sounds_bruh/" + meme + ".flac"
     #await message.channel.send("i wanna play " + memepath)
     print("trying to spawn new memeplayer")
@@ -250,8 +276,12 @@ async def gif(context, *args):
     response = await session.get(request)
     data = json.loads(await response.text())
     url = data['data'][0]['embed_url']
-    await message.channel.send("`" + query + "`")
-    await message.channel.send(url)
+    #print(message.channel.id)
+    memechannel = message.channel
+    if type(memechannel) is discord.DMChannel:
+        memechannel = bot.get_channel(wednesday_channel)
+    await memechannel.send("`" + query + "`")
+    await memechannel.send(url)
 
 @bot.command(brief="kills me bruh pls dont", description="bruh pls bruh i dont wanna die bruh\nto kill me you have to use my pid found in the `idontwannadie` file or printed out when i was born\nnot for normies")
 async def kys(context, *args):
@@ -267,10 +297,43 @@ async def kys(context, *args):
         session.close()
         await bot.close()
 
+@bot.command()
+async def shreksmite(context, *args):
+    global session
+    message = context.message
+    if not args:
+        await message.channel.send("wut are yu doin in mah swamp?! how dare you try to summon shrek to smite me you filthy casual")
+        return
+    password = args[0]
+    for arg in args[1:]:
+        password = password + arg
+    if password == shreks_holy_code:
+        await message.channel.send(getrandomshrekimage())
+        await message.channel.send("**its all ogre now**\n_*dies*_")
+        print("recieved kill command, sepeku time")
+        session.close()
+        await bot.close()
+
 @bot.command(brief="returns the integer id for your user bruh", description="returns the integer id for your user bruh, useful for discord debug n shit")
 async def getid(context):
     message = context.message
     await message.channel.send(str(message.author.id))
+
+#@bot.command(brief="search and find up to 25 memes bruh", description="shows the first 25 memes that contain substrings that exactly match bruh")
+#async def search(context, *args):
+#    message = context.message
+#    if not args:
+#        await context.channel.send("try using a search term bruh")
+#        return
+#    query = args[0]
+#    matches = findmemes(query, 25)
+#    matches.sort()
+#    if(matches == []):
+#        await message.channel.send("no sounds bruh")
+#    else:
+#        matches.insert(0, "")
+#        searchmessage = "\n".join(matches)
+#        await message.channel.send("bruh are you lookin for ```" + searchmessage + "```")
 
 @bot.command(brief="search and find up to 25 memes bruh", description="shows the first 25 memes that contain substrings that exactly match bruh")
 async def search(context, *args):
@@ -279,14 +342,23 @@ async def search(context, *args):
         await context.channel.send("try using a search term bruh")
         return
     query = args[0]
-    matches = findmemes(query, 25)
+    matches = findmemes(query, 500)
     matches.sort()
     if(matches == []):
         await message.channel.send("no sounds bruh")
     else:
-        matches.insert(0, "")
-        searchmessage = "\n".join(matches)
-        await message.channel.send("bruh are you lookin for ```" + searchmessage + "```")
+        #matches.insert(0, "")
+        #searchmessage = "\n".join(matches)
+        #await message.channel.send("bruh are you lookin for ```" + searchmessage + "```")
+        await message.channel.send("bruh are you lookin for")
+        chunksize = 50
+        chunks = [matches[i:i + chunksize] for i in range(0, len(matches), chunksize)]
+        for matcheschunk in chunks:
+            for string in matcheschunk:
+                string.prepend("`")
+                string.append("`")
+            mememessage = "\n".join(matcheschunk)
+            await message.channel.send(mememessage)
 
 @bot.command(brief="sends you all the memes in a massive DM, dont", description="this will tear into your DMs and send you all the available memes, which is so many memes bro. to actually send all the memes use\n##list memes\nthis is done to prevent your destruction")
 async def list(context, *args):
@@ -294,6 +366,7 @@ async def list(context, *args):
     if not args or args[0] != "memes":
         await message.channel.send("bruh i am so full of memes, if you are sure you want this use `##list memes`")
         return
+    await message.channel.send("you will not survive")
     await message.author.send("here are the available memes:")
     memefiles = getmemefiles()
     #split into chunks
@@ -302,7 +375,6 @@ async def list(context, *args):
     for memefileschunk in chunks:
         mememessage = "\n".join(memefileschunk)
         await message.author.send(mememessage)
-    await message.channel.send("you will not survive")
 
 @bot.command(brief="prints this message bruh", description="bruh dont you have something better to be doin")
 async def help(context, *args):
